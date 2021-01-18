d97050cca31377c65faa4f53abaf5a841a4f54fd
kiesha <kiesha.herman@gmail.com>
2021-01-17 21:22:23 -0800

    animate dotted climb path

--- src/pages/home/index-sc.tsx
+++ src/pages/home/index-sc.tsx
@@ -5,10 +5,7 @@ export const Container = styled.div`
   background: url('habrich-bg.jpg') center no-repeat;
   background-size: cover;
   position: relative;
-  /* width: 100%; */
-  /* max-width: 1200px; */
   height: 100vh;
-  /* min-height: 850px; */
 
   display: flex;
   justify-content: center;
@@ -27,15 +24,48 @@ export const Container = styled.div`
     padding: 1.2rem;
   }
 
-  img {
+  #kcImage {
     width: 120%;
     max-width: 750px;
     height: auto;
   }
 
   svg {
-    position: absolute;
     width: 100%;
     height: 100%;
+  }
+
+  #binoSVG {
+    position: absolute;
+  }
+`
+
+type ClimbPathProps = {
+  pathLength: number
+}
+
+export const ClimbPathContainer = styled.div<ClimbPathProps>`
+  width: 100%;
+  height: 100%;
+
+  #climbPathSVG {
+    .inner {
+      stroke: ${colors.terracotta};
+      stroke-width: 4;
+    }
+    .outer {
+      stroke: ${colors.darkGreen};
+      stroke-width: 8;
+    }
+    #motionPath {
+      stroke-width: 10;
+      stroke-dasharray: ${props => props.pathLength};
+    }
+
+    #heart {
+      stroke: ${colors.darkGreen};
+      stroke-width: 3;
+      fill: ${colors.terracotta};
+    }
   }
 `
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,8 +2,9 @@ import React, { useRef, useState, useEffect } from 'react'
 import gsap, { TimelineLite } from 'gsap'
 import { TextPlugin } from 'gsap/TextPlugin'
 
-import { Container } from './index-sc'
+import { Container, ClimbPathContainer } from './index-sc'
 import { mediaBreaks } from '../../theme'
+import { ReactComponent as ClimbPath } from './climbPath.svg'
 
 gsap.registerPlugin(TextPlugin)
 
@@ -14,52 +15,68 @@ const Home = (): JSX.Element => {
     vh: 850
   })
 
+  const [climbPathLength, setClimbPathLength] = useState(0)
   useEffect(() => {
     if (containerRef.current) {
-      setViewBox({
-        vw: containerRef.current.clientWidth,
-        vh: containerRef.current.clientHeight
-      })
+      const path: SVGPathElement | null = document.querySelector(
+        '#climbPathSVG path'
+      )
+      setClimbPathLength(path?.getTotalLength() || 0)
     }
   }, [containerRef.current])
+
+  // useEffect(() => {
+  //   if (containerRef.current) {
+  //     setViewBox({
+  //       vw: containerRef.current.clientWidth,
+  //       vh: containerRef.current.clientHeight
+  //     })
+  //   }
+  // }, [containerRef.current])
 
   useEffect(() => {
     if (containerRef.current) {
       const t1 = new TimelineLite({ delay: 1 })
 
-      t1.to('circle', { r: 850, duration: 2 })
       t1.fromTo(
-        'img',
-        { rotationX: 180, transformOrigin: '0% 100%' },
-        { rotationX: 0, duration: 2, ease: 'bounce' }
+        '#motionPath',
+        { strokeDashoffset: climbPathLength },
+        { strokeDashoffset: 0, duration: 2 }
       )
-      t1.fromTo(
-        'h1',
-        { display: 'none' },
-        {
-          display: 'block',
-          duration: 2,
-          text: "We're getting married!",
-          ease: 'none'
-        }
-      )
-      t1.to('h1', {
-        duration: 1,
-        text: 'Get ready for 2022!',
-        ease: 'none',
-        delay: 1.5
-      })
+
+      //     t1.to('circle', { r: 850, duration: 2 })
+      //     t1.fromTo(
+      //       'img',
+      //       { rotationX: 180, transformOrigin: '0% 100%' },
+      //       { rotationX: 0, duration: 2, ease: 'bounce' }
+      //     )
+      //     t1.fromTo(
+      //       'h1',
+      //       { display: 'none' },
+      //       {
+      //         display: 'block',
+      //         duration: 2,
+      //         text: "We're getting married!",
+      //         ease: 'none'
+      //       }
+      //     )
+      //     t1.to('h1', {
+      //       duration: 1,
+      //       text: 'Get ready for 2022!',
+      //       ease: 'none',
+      //       delay: 1.5
+      //     })
     }
   }, [containerRef.current])
 
-  const getCx = ((): { left: string; right: string } => {
+  const cx = ((): { left: string; right: string } => {
     if (viewBox.vw <= mediaBreaks.phone) {
       return { left: '37%', right: '63%' }
     }
     if (viewBox.vw <= mediaBreaks.tablet) {
       return { left: '42%', right: '58%' }
     } else {
-      return { left: '46%', right: '54%' }
+      return { left: '47%', right: '53%' }
     }
   })()
 
@@ -69,17 +86,22 @@ const Home = (): JSX.Element => {
         containerRef.current = ref
       }}
     >
-      <svg viewBox={`0 0 ${viewBox.vw} ${viewBox.vh}`}>
+      {/* <svg id="binoSVG" viewBox={`0 0 ${viewBox.vw} ${viewBox.vh}`}>
         <defs>
           <clipPath id="binoculars">
-            <circle cx={getCx.left} cy="50%" r="75" />
-            <circle cx={getCx.right} cy="50%" r="75" />
+            <circle cx={cx.left} cy="50%" r="75" />
+            <circle cx={cx.right} cy="50%" r="75" />
           </clipPath>
         </defs>
-      </svg>
+      </svg> */}
 
-      <h1></h1>
-      <img src="kandc-bw.png" alt="kiesha and colin" />
+      {/* <h1></h1> */}
+
+      <ClimbPathContainer pathLength={climbPathLength}>
+        <ClimbPath id="climbPathSVG" />
+      </ClimbPathContainer>
+
+      {/* <img id="kcImage" src="kandc-bw.png" alt="kiesha and colin" /> */}
     </Container>
   )
 }
