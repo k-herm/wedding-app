40bf89c5a590af10e9ba03d12e3ac43bcdfcb87f
kiesha <kiesha.herman@gmail.com>
2021-04-02 23:46:32 -0700

    allow restart, don't animate if cookie

--- src/pages/home/animation.tsx
+++ src/pages/home/animation.tsx
@@ -30,7 +30,12 @@ const travelPath = (pathLength: number) => {
     { strokeDashoffset: 0, duration: 2 },
     '-=2'
   )
-  t1.to('#pathMaskReveal', { display: 'block' }, '<0.1')
+  t1.fromTo(
+    '#pathMaskReveal',
+    { display: 'none' },
+    { display: 'block' },
+    '<0.1'
+  )
 
   t1.to('h1', { display: 'none', text: '' }, '+=0.5')
 
@@ -73,13 +78,15 @@ const surprise = (vw: number) => {
   return t1
 }
 
-export default (pathLength: number, vw: number): void => {
+export default (pathLength: number, vw: number): TimelineLite => {
   const t1 = new TimelineLite({ delay: 1 })
+  t1.addLabel('start')
 
-  t1.to('#binoculars circle', { r: 850, duration: 2 })
+  t1.to('h1', { display: 'none', text: '' })
+  t1.fromTo('#binoculars circle', { r: 75 }, { r: 850, duration: 2 })
   t1.add(travelPath(pathLength))
   t1.add(surprise(vw), '+=0.5')
-  t1.to('#pathMaskReveal', { opacity: 0, duration: 0.5 })
+  t1.fromTo('#pathMaskReveal', { opacity: 1 }, { opacity: 0, duration: 0.5 })
 
   t1.fromTo(
     '#kcImage',
@@ -99,4 +106,7 @@ export default (pathLength: number, vw: number): void => {
     ease: 'none',
     delay: 1.5
   })
+
+  t1.addLabel('end')
+  return t1
 }
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -5,24 +5,31 @@ import { ReactComponent as ClimbPath } from './climbPath.svg'
 import animateTimeline from './animation'
 
 const Home = (): JSX.Element => {
+  const [shouldAnimate, setShouldAnimate] = useState<boolean | null>(null)
+  useEffect(() => {
+    if (document.cookie.match(/kc_hasplayed=true/)) {
+      setShouldAnimate(false)
+    } else {
+      setShouldAnimate(true)
+      document.cookie = `kc_hasplayed=true;max-age=${
+        60 * 60 * 24 * 30
+      };secure;same-site`
+    }
+  }, [])
+
   const containerRef = useRef<HTMLElement | null>(null)
+  const [climbPathLength, setClimbPathLength] = useState(0)
   const [viewBox, setViewBox] = useState<{ vw: number; vh: number }>({
     vw: 1300,
     vh: 850
   })
 
-  const [climbPathLength, setClimbPathLength] = useState(0)
   useEffect(() => {
     if (containerRef.current) {
       const path: SVGPathElement | null = document.querySelector(
         '#climbPathSVG path'
       )
       setClimbPathLength(path?.getTotalLength() || 0)
-    }
-  }, [containerRef.current])
-
-  useEffect(() => {
-    if (containerRef.current) {
       setViewBox({
         vw: containerRef.current.clientWidth,
         vh: containerRef.current.clientHeight
@@ -32,9 +39,20 @@ const Home = (): JSX.Element => {
 
   useEffect(() => {
     if (containerRef.current) {
-      animateTimeline(climbPathLength, viewBox.vw)
+      const t1 = animateTimeline(climbPathLength, viewBox.vw)
+      if (shouldAnimate) {
+        t1.play('start')
+        t1.eventCallback('onComplete', () => setShouldAnimate(false))
+      } else if (shouldAnimate === false) {
+        t1.seek('end')
+      } else {
+        t1.seek('start')
+        t1.pause()
+      }
     }
-  }, [containerRef.current])
+  }, [shouldAnimate])
+
+  const onRestart = () => setShouldAnimate(true)
 
   const cx = ((): { left: string; right: string } => {
     if (viewBox.vw <= mediaBreaks.phone) {
@@ -70,6 +88,13 @@ const Home = (): JSX.Element => {
 
       <div id="emoji">😱</div>
       <img id="kcImage" src="kandc-bw.png" alt="kiesha and colin" />
+
+      <img
+        id="reloadIcon"
+        src="reload-icon.png"
+        alt="reload icon"
+        onClick={onRestart}
+      />
     </Container>
   )
 }
