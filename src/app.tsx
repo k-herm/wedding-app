436eb01ab174ca1dfee64e56cf39e4e5f0142a42
kiesha <kiesha.herman@gmail.com>
2021-04-11 21:08:24 -0700

    preload font, add suspense/lazy

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { ThemeProvider } from '@material-ui/core/styles'
 import CssBaseline from '@material-ui/core/CssBaseline'
@@ -6,38 +6,41 @@ import CssBaseline from '@material-ui/core/CssBaseline'
 import PrivateRoute from './utils/private-route'
 import { AuthProvider } from './utils/auth-context'
 
-import Home from './pages/home'
-import Admin from './pages/admin'
-import Rsvp from './pages/rsvp'
-import PageDoesNotExist from './pages/404'
+const Home = lazy(() => import('./pages/home'))
+const Admin = lazy(() => import('./pages/admin'))
+const Rsvp = lazy(() => import('./pages/rsvp'))
+const PageDoesNotExist = lazy(() => import('./pages/404'))
 
 import { theme } from './theme'
 
 const App = (): JSX.Element => (
-  <ThemeProvider theme={theme}>
-    <CssBaseline />
-    <AuthProvider>
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
+  // TO DO add loading state
+  <Suspense fallback={<></>}>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AuthProvider>
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
 
-          <Route path="/rsvp">
-            <Rsvp />
-          </Route>
+            <Route path="/rsvp">
+              <Rsvp />
+            </Route>
 
-          <PrivateRoute path="/admin" permission="admin">
-            <Admin />
-          </PrivateRoute>
+            <PrivateRoute path="/admin" permission="admin">
+              <Admin />
+            </PrivateRoute>
 
-          <Route path="*">
-            <PageDoesNotExist />
-          </Route>
-        </Switch>
-      </Router>
-    </AuthProvider>
-  </ThemeProvider>
+            <Route path="*">
+              <PageDoesNotExist />
+            </Route>
+          </Switch>
+        </Router>
+      </AuthProvider>
+    </ThemeProvider>
+  </Suspense>
 )
 
 export default App
